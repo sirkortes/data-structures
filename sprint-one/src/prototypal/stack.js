f3a20efb4c226fe2b03edc2162690380c8e3f26f
Hack Reactor Student <student@hackreactor.com>
2017-03-29 16:43:05 -0700

    add partial solutions for prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(Stack.prototype);
+  instance.count = 0;
+  instance.storage = {};
+  return instance;
 };
 
 var stackMethods = {};
 
+Stack.prototype.size = function() {
+	return this.count;
+}
 
+Stack.prototype.push = function(value){
+	this.storage[this.count] = value;
+	this.count++;
+}
+
+Stack.prototype.pop = function(){ 
+	var deleted = 'Oops';
+	if ( this.count > 0 ){ 
+		deleted = this.storage[this.count];
+		delete this.storage[this.count];
+		this.count--;
+	}
+	return deleted;
+}
