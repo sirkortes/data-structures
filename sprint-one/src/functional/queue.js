1fbcc1a600a52681c7e3d2ffd51275b570a6c060
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:19:01 -0700

    Complete Stack and Queue for Functional Pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,19 +4,31 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var counter = 0;
+  var qchange = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
     counter++;
   };
 
   someInstance.dequeue = function() {
-    if( counter > 0 ){ counter--; }
+    
+    if( counter - qchange > 0 ){ 
+      var deleted = storage[qchange];
+      delete storage[qchange];
+      // counter--;
+      qchange++;
+      return deleted;
+    }
+    return 'negative, bruh';
   };
 
   someInstance.size = function() {
-    return counter;
+    return counter - qchange;
   };
+
+
 
   return someInstance;
 };
