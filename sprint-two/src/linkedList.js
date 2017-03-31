c3d2f455ee214c75457435e3edb9a6c728cec885
Hack Reactor Student <student@hackreactor.com>
2017-03-30 20:00:04 -0700

    Progress on Linked Lists up to Contains Function

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,26 +1,68 @@
 var LinkedList = function() {
+
+  // console.log(this);
+
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    var node = new Node(value);
+
+    // only for first
+    if ( this.head === null ){ 
+      this.head = node; 
+    }
+
+    // only for second
+    if ( this.head !== null && this.head.next ){
+      this.head.next = node;
+    }
+
+    if ( this.tail !== null ){
+      this.tail.next = node;
+    }
+
+    // every other node
+    this.tail = node; 
+
+    console.log("added, now this list: ", this);
   };
 
   list.removeHead = function() {
+
+    if ( this.head !== null ){
+
+      console.log("head: ",this.head);
+      var result = this.head.value;
+      this.head = this.head.next;
+      console.log("remove head will return:",result);
+      console.log("after remove:",this);
+      return result;
+    } else {
+      console.log("this.head === "+this.head);
+    }
+
   };
 
   list.contains = function(target) {
+
+
+
   };
 
+  console.log("*********",list);
   return list;
 };
+
 
 var Node = function(value) {
   var node = {};
 
   node.value = value;
   node.next = null;
-
+  
   return node;
 };
 
