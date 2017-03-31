105347025f97bf16f89c46f4ded4a8d7ba57836b
Hack Reactor Student <student@hackreactor.com>
2017-03-31 14:49:42 -0700

    Removed comments and logs from Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,14 +1,11 @@
 var Tree = function(value) {
 
   var newTree = {};
-  _.extend(newTree, treeMethods);
   newTree.value = value;
   newTree.children = [];  
+  _.extend(newTree, treeMethods);
 
-  console.log("*******");
   return newTree;
-
-
 };
 
 var treeMethods = {};
@@ -22,46 +19,20 @@ treeMethods.addChild = function(value) {
 
 treeMethods.contains = function(target, node) {
 	
-	// Dat Base Case
-	console.log("calling contains",target,node);
-
 	if ( this.value === target ){
-		console.log("target === ",this.value);
 		return true;
 
 	} else if ( this.children.length > 0 ) {
 
-		// result
 		var result = false;
 
 		for (var i = 0; i < this.children.length; i++) {
-			// var test = this.contains.call(this.children[i],target);
-			// if ( test ) { result = test; }
 			result = result ? result : this.contains.call(this.children[i],target);
 		}
 
 		return result;
-		// // result 
-		// var result = false;
-		// // while result is false and index is equal to lenght
-		// while ( !result ) {
-
-		//     this.children.forEach(function(child, index){
-		// 		result = child.contains(target);
-		// 	});
-		// 	break;
-	 //    }
-		// // return result
-		// console.log("had children, returned: ",result);
-		// return result;
-
-		// check 0
-			// 	if 0 != target && 1 !== null
-				// check 1
-
 		
 	} else {
-		console.log("else false");
 		return false;
 	}
 };
