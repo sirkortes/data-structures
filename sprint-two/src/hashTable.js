f44e12527b55ffdeb472d5a8dcc93d83ade40565
Hack Reactor Student <student@hackreactor.com>
2017-04-01 10:56:26 -0700

    Finish Minimum Requirements of Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,36 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var result = {};
+  var currentValue = this._storage.get(index)
+	if ( currentValue ){
+    	result = currentValue;
+	}
+  result[k] = v;
+  this._storage.set(index, result);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var currentValue = this._storage.get(index);
+  return currentValue[k];
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var currentValue = this._storage.get(index);
+  delete currentValue[k];
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert: O(1),
+ retrieve: O(1),
+ remove: O(1)
  */
 
 
